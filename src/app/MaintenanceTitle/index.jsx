19c5e35e0fbb1be6f744aa153c4cdb222bd52495
Mauricio2085 <maoca@gmail.com>
2023-12-06 13:09:29 -0500

    Initial commit

---
+++ src/app/MaintenanceTitle/index.jsx
@@ -0,0 +1,7 @@
+const MaintenanceTitle = () => {
+return (
+    <h1 className="text-2xl bg-[rgb(214, 219, 220)]">La mejor app de mantenimiento que vas a utilizar!!</h1>
+)
+}
+
+export { MaintenanceTitle};
